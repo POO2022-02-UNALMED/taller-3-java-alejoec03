3df97a8d4e3c545a87c37f2ecd6d7b4186552822
alejoec03 <juespinosac@unal.edu.co>
2022-08-29 11:28:08 -0500

    :+1:

---
+++ src/test/java/test/Control.java
@@ -0,0 +1,38 @@
+package televisores;
+
+public class Control {
+
+	private TV tv;
+
+	public TV getTv() {
+		return tv;
+	}
+
+	public void setTv(TV tv) {
+		this.tv = tv;
+	}
+	
+	public void turnOn() {
+		this.tv.turnOn();
+	}
+	public void turnOff() {
+		this.tv.turnOff();
+	}
+	
+	public void canalUp() {
+		this.tv.canalUp();
+	}
+	public void canalDown() {
+		this.tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		this.tv.volumenUp();
+	}
+	public void volumenDown() {
+		this.tv.volumenDown();
+	}
+	public void setCanal(int canal) {
+		this.tv.setCanal(canal);
+	}
+}
